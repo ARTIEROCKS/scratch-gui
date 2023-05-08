a3f4fa2d1ae9a97a86f0eccd432b94b059456151
Luis Eduardo Imbernón <imbernon@gmail.com>
2023-05-08 20:17:13 +0200

    Added the paths for the different web services.

--- src/lib/artie-api.js
+++ src/lib/artie-api.js
@@ -13,11 +13,11 @@ const _pedagogicalInterventionWebServiceUrl = 'https://pre-prod.artie.rocks:8443
 const _pedagogialWebUrl = 'https://pre-prod.artie.rocks:8443';
 const _apiKey = 'k6siZlG9OZGyMorpmSUeYo87ebfsN7s0';
 
-const _pedagogicalSoftwarePath = '/api/v1/pedagogicalsoftware';
+const _pedagogicalSoftwarePath = '/pedagogicalsoftware';
 const _usersPath = '/users';
 const _studentsPath = '/students';
 const _exercisesPath = '/exercises';
-const _sensorPath = '/api/v1/sensor';
+const _sensorPath = '/sensor';
 
 
 const _createArtieBlockFromTempBlock = (tempBlock) => ({id: tempBlock.id, elementName: tempBlock.elementName, elementFamily: tempBlock.elementFamily, next: tempBlock.next, inputs: tempBlock.inputs, nested: tempBlock.nested, previous: tempBlock.previous, parent: tempBlock.parent});
