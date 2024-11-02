499d194575d9861cc363e239f46f60c24df50030
Luis Eduardo Imbernón <imbernon@gmail.com>
2024-11-02 12:05:45 +0100

    Wecbam component has been fixed for being able to get the webcam images.

--- src/components/artie-webcam-recorder/artie-webcam-recorder-component.jsx
+++ src/components/artie-webcam-recorder/artie-webcam-recorder-component.jsx
@@ -1,208 +1,100 @@
-import {useEffect, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
+import {useEffect, useRef, useState, forwardRef, useImperativeHandle} from 'react';
 
-const ArtieWebcamRecorderComponent = props => {
-
-    const streamRef = useRef();
-    const streamRecorderRef = useRef();
-    const [isRecording, setIsRecording] = useState(false);
-    const [audioSource, setAudioSource] = useState('');
-    const [videoSource, setVideoSource] = useState('');
+const ArtieWebcamRecorderComponent = forwardRef(({
+    userName,
+    password,
+    student,
+    sensorObjectType,
+    sensorName,
+    send
+}, ref) => {
+    const mediaRecorderRef = useRef(null);
     const [fromDate, setFromDate] = useState(null);
-    const [toDate, setToDate] = useState(null);
-    const chunks = useRef();
-    const [error, setError] = useState(null);
-
-    const dateOptions = {year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        timeZone: 'UTC',
-        timeZoneName: 'short'};
 
     const startRecording = () => {
-        if (isRecording) {
-            return;
+        if (mediaRecorderRef.current) { // Check if webcam is ready
+            setFromDate(new Date()); // Initial fromDate for the first segment
+            mediaRecorderRef.current.start();
+        } else {
+            console.warn('Webcam is not ready. Please wait...');
         }
-        if (!streamRef.current){
-            return;
-        }
-        streamRecorderRef.current = new MediaRecorder(streamRef.current);
-        streamRecorderRef.current.start();
-        setToDate(null);
-        setFromDate(new Date().toLocaleDateString('es-ES', dateOptions));
-        streamRecorderRef.current.ondataavailable = function (event){
-            if (chunks.current.length > 0) {
-                chunks.current.push(event.data);
-            } else {
-                chunks.current = [event.data];
-            }
-        };
-        setIsRecording(true);
     };
 
     const stopRecording = () => {
-        if (!streamRecorderRef.current){
-            return;
+        if (mediaRecorderRef.current && mediaRecorderRef.current.state === 'recording') {
+            mediaRecorderRef.current.stop();
         }
-        streamRecorderRef.current.stop();
-        setToDate(new Date().toLocaleDateString('es-ES', dateOptions));
-        setIsRecording(false);
     };
 
-    /**
-     * Use Effect for getting the states from the local storage and sets in the component
-     */
-    useEffect(() => {
-
-        setIsRecording(window.localStorage.getItem('webcamRecorder_isRecording') === 'true');
-        setAudioSource(window.localStorage.getItem('webcamRecorder_audioSource'));
-        setVideoSource(window.localStorage.getItem('webcamRecorder_videoSource'));
-        setFromDate(window.localStorage.getItem('webcamRecorder_fromDate'));
-        setFromDate(window.localStorage.getItem('webcamRecorder_toDate'));
-        setError(window.localStorage.getItem('webcamRecorder_error'));
-
-    }, []);
-
-    /**
-     * Use Effect for setting the states to the local storage
-     */
-    useEffect(() => {
-        window.localStorage.setItem('webcamRecorder_isRecording', isRecording);
-        window.localStorage.setItem('webcamRecorder_props_recording', props.artieWebcam.recording);
-        window.localStorage.setItem('webcamRecorder_audioSource', audioSource);
-        window.localStorage.setItem('webcamRecorder_videoSource', videoSource);
-        window.localStorage.setItem('webcamRecorder_fromDate', fromDate);
-        window.localStorage.setItem('webcamRecorder_toDate', toDate);
-        window.localStorage.setItem('webcamRecorder_error', error);
-    }, [isRecording, audioSource, videoSource, fromDate, toDate, error]);
+    useImperativeHandle(ref, () => ({
+        startRecording,
+        stopRecording
+    }));
 
     useEffect(() => {
-        if (isRecording) {
-            return;
-        }
-        if (chunks !== undefined && chunks.current !== undefined && chunks.current.length === 0){
-            return;
-        }
-        const blob = new Blob(chunks.current, {
-            type: 'video/x-matroska;codecs=avc1'
-        });
+        const handleDataAvailable = event => {
+            if (event.data.size > 0) {
+                const toDate = new Date();
+                const jsonBlob = new Blob([JSON.stringify(event.data)], {type: 'application/json'});
 
-        const reader = new FileReader();
-        reader.onload = function () {
-            const jsonBlob = reader.result;
+                send({
+                    userName,
+                    password,
+                    student,
+                    sensorObjectType,
+                    sensorName,
+                    data: jsonBlob,
+                    fromDate,
+                    toDate
+                });
 
-            // Sends all the information to the API
-            // eslint-disable-next-line max-len
-            props.send(props.userName, props.password, props.student, props.sensorObjectType, props.sensorName, jsonBlob,
-                fromDate, toDate);
-
-            chunks.current = [];
+                // Update fromDate to prepare for the next segment
+                setFromDate(new Date());
+            }
         };
-        reader.readAsDataURL(blob);
 
-    }, [isRecording]);
+        const initWebcam = async () => {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({video: true});
+                mediaRecorderRef.current = new MediaRecorder(stream, {mimeType: 'video/webm'});
+                mediaRecorderRef.current.ondataavailable = handleDataAvailable;
 
-    // Effect to stop and then to start again the video and audio recording
-    useEffect(() => {
-
-        // Creates the interval function to send the video every x seconds
-        const interval = setInterval(() => {
-
-            // eslint-disable-next-line max-len
-            const isRecordingLocal = (window.localStorage.getItem('webcamRecorder_isRecording') === 'true');
-            const propsRecording = (window.localStorage.getItem('webcamRecorder_props_recording') === 'true');
-
-            if (isRecordingLocal && propsRecording) {
-                stopRecording();
-            } else if (propsRecording) {
-                startRecording();
-            }
-        }, 5000);
-        return () => clearInterval(interval);
-    }, []);
-
-    useEffect(() => {
-        if (props.artieWebcam.recording) {
-            startRecording();
-        } else {
-            stopRecording();
-        }
-    }, [props.artieWebcam.recording]);
-
-    useEffect(() => {
-        async function prepareStream () {
-
-            async function gotStream (stream) {
-                if (streamRef !== undefined) {
-                    streamRef.current = stream;
-                }
-            }
-
-            function gotDevices (deviceInfos){
-                const _audioSourceOptions = [];
-                const _videoSourceOptions = [];
-
-                for (const device of deviceInfos){
-                    if (device.kind === 'audioinput'){
-                        _audioSourceOptions.push(device.deviceId);
-                    } else if (device.kind === 'videoinput') {
-                        _videoSourceOptions.push(device.deviceId);
+                // Añadir un retraso antes de llamar a startRecording
+                setTimeout(() => {
+                    if (mediaRecorderRef.current && mediaRecorderRef.current.state !== 'recording') {
+                        startRecording();
                     }
-                }
-
-                // We take the first options for audio and video
-                if (_audioSourceOptions.length > 0) {
-                    setAudioSource(_audioSourceOptions[0]);
-                }
-                if (_videoSourceOptions.length > 0) {
-                    setVideoSource(_videoSourceOptions[0]);
-                }
+                }, 1000); // Espera de 1 segundo
+            } catch (error) {
+                console.error('Error accessing webcam:', error);
             }
+        };
 
-            async function getStream () {
-                if (streamRef.current) {
-                    streamRef.current.getTracks().forEach(track => {
-                        track.stop();
-                    });
-                }
+        initWebcam();
+    }, [userName, password, student, sensorObjectType, sensorName, send]);
 
-                const constraints = {
-                    audio: {deviceId: audioSource === '' ? undefined : {exact: audioSource}},
-                    video: {deviceId: videoSource === '' ? undefined : {exact: videoSource}}
-                };
+    useEffect(() => {
+        if (mediaRecorderRef.current && mediaRecorderRef.current.state === 'recording') {
+            const interval = setInterval(() => {
+                mediaRecorderRef.current.requestData();
+            }, 5000);
 
-                try {
-                    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-                    await gotStream(stream);
-                } catch (err) {
-                    setError(err);
-                }
-
-            }
-            const getDevices = () => navigator.mediaDevices.enumerateDevices();
-
-            const mediaDevices = await getDevices();
-            await getStream();
-            gotDevices(mediaDevices);
+            return () => clearInterval(interval);
         }
-        prepareStream();
-    }, []);
+    }, [fromDate]);
+    
+    ArtieWebcamRecorderComponent.displayName = 'ArtieWebcamRecorderComponent';
+    return null; // No JSX returned since it's a functional, invisible component
+});
 
-    return null;
-};
 ArtieWebcamRecorderComponent.propTypes = {
-    artieWebcam: PropTypes.shape({
-        recording: PropTypes.bool.isRequired
-    }).isRequired,
-    send: PropTypes.func.isRequired,
     userName: PropTypes.string.isRequired,
     password: PropTypes.string.isRequired,
-    student: PropTypes.string.isRequired,
+    student: PropTypes.object.isRequired,
     sensorObjectType: PropTypes.string.isRequired,
-    sensorName: PropTypes.string.isRequired
+    sensorName: PropTypes.string.isRequired,
+    send: PropTypes.func.isRequired
 };
 
 export default ArtieWebcamRecorderComponent;
--- src/containers/artie-webcam-recorder.jsx
+++ src/containers/artie-webcam-recorder.jsx
@@ -9,27 +9,24 @@ import {sendSensorInformation} from '../lib/artie-api';
 class ArtieWebcamRecorder extends React.Component{
     constructor (props) {
         super(props);
-        bindAll(this, ['onHandleSendSensorInformation', 'removeAllItems']);
+        bindAll(this, ['onHandleSendSensorInformation']);
+        this.recorderRef = React.createRef();
     }
-    componentWillMount () {
-        this.removeAllItems();
-    }
-    componentWillUnmount () {
-        this.removeAllItems();
+
+    componentDidUpdate (prevProps) {
+        if (prevProps.artieWebcam.recording !== this.props.artieWebcam.recording) {
+            if (this.props.artieWebcam.recording) {
+                this.recorderRef.current.startRecording();
+            } else {
+                this.recorderRef.current.stopRecording();
+            }
+        }
     }
 
     onHandleSendSensorInformation (userName, password, student, sensorObjectType, sensorName, data, fromDate, toDate) {
         sendSensorInformation(userName, password, student, sensorObjectType, sensorName, data, fromDate, toDate);
     }
-    removeAllItems () {
-        window.localStorage.removeItem('webcamRecorder_isRecording');
-        window.localStorage.removeItem('webcamRecorder_props_recording');
-        window.localStorage.removeItem('webcamRecorder_audioSource');
-        window.localStorage.removeItem('webcamRecorder_videoSource');
-        window.localStorage.removeItem('webcamRecorder_fromDate');
-        window.localStorage.removeItem('webcamRecorder_toDate');
-        window.localStorage.removeItem('webcamRecorder_error');
-    }
+
     render () {
         if (this.props.artieLogin !== null &&
             this.props.artieLogin.user !== null && this.props.artieLogin.user.login !== null &&
@@ -39,13 +36,13 @@ class ArtieWebcamRecorder extends React.Component{
                 this.props.artieLogin.currentStudent.recordFace)) {
 
             return (<ArtieWebcamRecorderComponent
+                ref={this.recorderRef}
                 userName={this.props.artieLogin.user.login}
                 password={this.props.artieLogin.user.password}
                 student={this.props.artieLogin.currentStudent}
                 sensorObjectType={'VIDEO'}
                 sensorName={'SCRATCH_WEBCAM'}
                 send={this.onHandleSendSensorInformation}
-                artieWebcam={this.props.artieWebcam}
             />);
         }
         return null;
@@ -56,6 +53,7 @@ const mapStateToProps = state => ({
     artieLogin: state.scratchGui.artieLogin,
     artieWebcam: state.scratchGui.artieWebcam
 });
+
 ArtieWebcamRecorder.propTypes = {
     artieLogin: PropTypes.object,
     artieWebcam: PropTypes.object
