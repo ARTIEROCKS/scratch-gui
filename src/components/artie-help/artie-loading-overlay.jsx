824a469006248a7b6cf8466669a811e1dcd16b5f
Luis Eduardo Imbernón <imbernon@gmail.com>
2025-10-17 10:36:33 +0200

    Refactor ArtieLoadingOverlay to use ReactDOM.createPortal for improved rendering

--- src/components/artie-help/artie-loading-overlay.jsx
+++ src/components/artie-help/artie-loading-overlay.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-multiple-empty-lines */
 import classNames from 'classnames';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import {FormattedMessage} from 'react-intl';
 
@@ -10,21 +11,24 @@ import styles from './artie-loading-overlay.css';
 const ArtieLoadingOverlay = ({
     className
 }) => (
-    <div className={classNames(styles['artie-loading-overlay'], className)}>
-        <div className={styles['artie-loading-overlay__content']}>
-            <Spinner
-                large
-                level="info"
-            />
-            <div className={styles['artie-loading-overlay__text']}>
-                <FormattedMessage
-                    defaultMessage="Calculando tu ayuda..."
-                    description="Overlay text shown while waiting for ARTIE help response"
-                    id="gui.artie.loadingHelp"
+    ReactDOM.createPortal(
+        <div className={classNames(styles['artie-loading-overlay'], className)}>
+            <div className={styles['artie-loading-overlay__content']}>
+                <Spinner
+                    large
+                    level="info"
                 />
+                <div className={styles['artie-loading-overlay__text']}>
+                    <FormattedMessage
+                        defaultMessage="Calculando tu ayuda..."
+                        description="Overlay text shown while waiting for ARTIE help response"
+                        id="gui.artie.loadingHelp"
+                    />
+                </div>
             </div>
-        </div>
-    </div>
+        </div>,
+        document.body
+    )
 );
 
 ArtieLoadingOverlay.propTypes = {
